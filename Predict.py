5edd5f64111602909b5d25a38b91925e381cf853
Ressnn <42896924+Ressnn@users.noreply.github.com>
2020-01-06 21:06:43 -0500

    Add files via upload

--- Predict.py
+++ Predict.py
@@ -1,43 +1,55 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov  8 09:28:28 2018
+Created on Thu Nov  7 09:28:28 2019
 
 @author: ASUS
 """
-
+# In[]
+#All of Our Other File Imports - For Organization because the data can be tinkered with faster this way
 import DataGetter
 import DataShaper
+
 import numpy as np
+#Numpy: for general number processing and for the np.array datatype which is easy to index
 import pandas as pd
+#Pandas: for data processing
 import keras
 from keras.models import Sequential
-from keras.layers import Dense,LSTM,GRU,CuDNNLSTM,CuDNNGRU,Dropout
+from keras.layers import Dense,LSTM,GRU,CuDNNLSTM,CuDNNGRU,Dropout,Flatten
 from keras.activations import softsign,selu,elu,relu
+import matplotlib.pyplot as plt
+#Keras:to make and train NN
+
+# In[]
+
 #Variables go here
 #Shifting around variables here is reccomended
-tickers  = ['NVDA']
-start_date = '2015-01-01'
-end_date = '2018-11-20'
-target_size = 750
-splits = 5
+tickers  = [str(input("Ticker to Predict? "))]
+#When whould the date of input be?
+
+
+
+
+start_date = '2017-08-10'
+end_date = str(input("When should the predictions start? yy-mm-dd "))
+target_size = 400
+splits = 10
+
+
 
 #Model settings//Change if you know what you are doing
 test_list_size = 1
 epochs = int(100)
 eb = int(6)
 nsub = int(target_size/splits)
-import matplotlib.pyplot as plt
+
 
 # In[]
-class Data():
-    stock_data = []
-    trend_data = []
-    stock_data, trend_data = DataGetter.Get_Data(tickers,start_date,end_date)
-
-# In[]
+stock_data = []
+trend_data = []
+x, r = DataGetter.Get_Data(tickers,start_date,end_date)
 # The market's close value is the third one to iloc
-x = Data.stock_data
-r = Data.trend_data
+
 # In[]
 nx = []
 nr = []
@@ -58,16 +70,11 @@ model = Sequential()
 
 model = Sequential()
 try:
-    model.add(CuDNNLSTM(1000,return_sequences=True))
-    model.add(Dropout(.1))
-    model.add(CuDNNLSTM(500,return_sequences=True))
-    model.add(Dropout(.1))
-    model.add(CuDNNLSTM(300,return_sequences=True))
-    model.add(Dropout(.1))
-    model.add(Dense(500,activation='selu'))
-    model.add(Dense(300))
-    model.add(Dropout(.1))
-    model.add(Dense(100))
+    model.add(CuDNNLSTM(75,return_sequences=True))
+    model.add(CuDNNLSTM(40,return_sequences=True))
+    model.add(Dense(125,activation='tanh'))
+    model.add(Dense(45))
+    model.add(Dropout(.2))
     model.add(Dense(6))
 except:
     print('cudnn load failed!')
@@ -79,45 +86,67 @@ except:
     model.add(Dense(300))
     model.add(Dense(6))
 model.compile(loss='mse', optimizer='adam')
+
+
 # In[]
 shape = Batched[0][0].shape
 new_shape = (1,shape[0],shape[1])
 for i in range(0,epochs):
     model.fit(np.array(Batched[0][0]).reshape(new_shape),np.array(Batched[0][0]).reshape(new_shape))
 # In[]
-model.load_weights('ModelW.h5')
+model.load_weights('ModelW2.h5')
 
 # In[]
-ai_see = model.predict(np.array(Batched[0][0]).reshape(new_shape))
-ai_predict = model.predict(np.array(Batched[0][1]).reshape(new_shape))
+
+sn0 = -1
+sn1 = (-nsub*1)-1
+sn2 = (-nsub*2)-1
+
+
+
+ai_see = model.predict(np.array(x[0])[:,sn2:sn1].reshape(new_shape))
+ai_predict = model.predict(np.array(x[0])[:,sn1:sn0].reshape(new_shape))
 ai_see = pd.DataFrame(ai_see[0]).transpose()
 ai_predict = pd.DataFrame(ai_predict[0]).transpose()
-ai_see = ai_see.iloc[2,:].tolist()
-ai_predict = ai_predict.iloc[2,:].tolist()
-batched_1 = Batched[0][0][2].values.tolist()
-batched_2 = Batched[0][1][2].values.tolist()
+ai_see = ai_see.iloc[3,:].tolist()
+ai_predict = ai_predict.iloc[3,:].tolist()
+batched_1 = np.transpose(np.array(x[0])[:,sn2:sn1]).reshape(new_shape)
+batched_2 = np.transpose(np.array(x[0])[:,sn1:sn0]).reshape(new_shape)
 # In[]
 dc = 2
 import PredictionUtils
-batched_2 = PredictionUtils.UndoDiff(batched_2)
+batched_2 
 ar_see = []
 for i in ai_see:
-    ar_see.append(i/3.5)
+    ar_see.append(i)
 ar_pred = []
 for i in ai_predict:
-    ar_pred.append(i/3.5)
+    ar_pred.append(i)
 ai_see = PredictionUtils.UndoDiff(ar_see)
 ai_predict = PredictionUtils.UndoDiff(ar_pred)
-plt.plot(batched_2)
-plt.plot(ai_see)
+
+
+add = np.array(batched_2)[0,-1 ,3]
+
 ai_p_s = []
 for i in ai_predict:
-    ai_p_s.append(i+batched_2[-1])
+    ai_p_s.append(i)
 
 range_list = []
 for i in range(int(target_size-(target_size/splits)),target_size):
     for a in range(0,splits-2):
         i = i-(target_size/splits)
     range_list.append(i)
-    
-plt.plot(range_list,ai_p_s)
+# In[]
+plt.plot(np.array(batched_2)[0,:,3])
+plt.plot(np.array(DataShaper.UndoDiff(ar_see)))
+
+final_pred_list = np.array(ar_pred)
+final_amount = (final_pred_list-np.mean(final_pred_list))/np.std(final_pred_list)
+dt = DataShaper.UndoDiff(final_amount)
+plt.plot(range_list,dt)
+plt.plot(range_list,DataShaper.UndoDiff(final_pred_list))
+# In[]
+print("One Day Prediction : (Normal:"+ str(final_pred_list[0])+" Standardized:"+ str(final_amount[0])+")")
+print("Two Day Prediction : (Normal:"+ str(final_pred_list[1])+" Standardized:"+ str(final_amount[1])+")")
+
